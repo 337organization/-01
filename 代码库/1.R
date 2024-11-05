533a442d2b8bd5f7f6cb1a5306ea2d2730c6f2c9
harmonious-2024 <2271275852@qq.com>
2024-11-05 22:51:16 +0800

    作业题

---
+++ 代码库/1.R
@@ -0,0 +1,55 @@
+forq()
+'demo()'
+”hello“
+"hello world"
+1+1
+x=1
+x+1
+log(3)
+?log
+d1<-date.frame(v1=x,v2=z)
+help.start()
+plot(MSales~Comments,
+data=df2)
+help.start()
+cin
+li<-list(x1,x2,A1)
+> x=1
+utils:::menuShowCRAN()
+> x+1
+install.packages("readxl")
+library(readxl)
+class(1)
+x<-1:6
+y<-c(2,5,7,1,9,4.5)
+z<-rep("A",6)
+x+1
+x*2
+x*x
+y>2
+z
+rep(y,6)
+y[1]
+y[c(2,4)]
+y[c(2:4)]
+x<-c(1,3.5,2.7,6,4.8)
+for (i in 1:5){
+y<-x[i]
+z<-0
+z<-z+y}
+z
+z<-0
+> x<-c(1,3.5,2.7,6,4.8)
+> for (i in 1:5){
++ y<-x[i]
++ z<-0
++ z<-z+y}
+z<-0
+x<-c(1,3.5,2.7,6,4.8)
+x<-c(1,3.5,2.7,6,4.8)
+ for (i in 1:5){
+y<-x[i]
+z<-z+y}
+z
+z/6
+source("E:\\我的\\work\\R\\测试.R")
---
+++ 代码库/第二题手写循环.R
@@ -0,0 +1,198 @@
+q()
+'demo()'
+1+1
+q()
+q()
+1+1
+save.image("E:\\
+ z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+ for (i in 1:5){
+ for (j in 1:i){
+ if(x[j]>x[j+1]){
+ t<-x[j]
+ x[j]<-x[j+1]
+ x[j+1]<-t
+ t<-x[j]}
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+s
+1
+x
+end
+stop
+stop()
+quit
+q()
+q()
+q()
+q()
+q()
+q()
+sb
+q()
+ z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+ for (i in 1:5){
+ for (j in 1:i-1){
+ if(x[j]>x[j+1]){
+ t<-x[j]
+ x[j]<-x[j+1]
+ x[j+1]<-t
+ t<-x[j]}
+}}
+void bubbleSort(vector<double>&  arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++
+            ) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+int x=0
+int x<-0
+x<-0
++  t<-x[j]
++  x[j]<-x[j+1]
++  x[j+1]<-t
++  t<-x[j]}
++ }}
+if(x[j]>x[j+1]){
++  t<-x[j]
++  x[j]<-x[j+1]
++  x[j+1]<-t
++  t<-x[j]}
++ }}
+if(x[j]>x[j+1]){
+t<-x[j]
+x[j]<-x[j+1]
+x[j+1]<-t}
+x<-c(1,3.5,2.7,6,4.8)
+if(x[j]>x[j+1]){
+t<-x[j]
+x[j]<-x[j+1]
+x[j+1]<-t
+stop()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+q()
+x
+}
+ z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+ for (i in 1:5){
+ for (j in 1:i-1){
+ if(x[j]>x[j+1]){
+ t<-x[j]
+ x[j]<-x[j+1]
+ x[j+1]<-t
+ }
+}}
+ z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+for (j in 1:i-1){
+ if(x[j]>x[j+1]){
+ t<-x[j]
+ x[j]<-x[j+1]
+ x[j+1]<-t
+ }
+}
+swap()
+replace()
+x<-0
+y<-1
+x=replace(y)
+x<-replace(y)
+q()
+library()
+z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+ for (i in 1:5){
+ for (j in 1:i-1){
+ if(x[j]>x[j+1]){
+ t<-x[j]
+ x[j]<-x[j+1]
+ x[j+1]<-t
+ }
+x
+stop
+w
+www
+if (a
+ z<-0
+ x<-c(1,3.5,2.7,6,4.8)
+ y<-0
+ for (i in 1:4){
+if(x[i]>x[i+1]){
+t<-x[i]
+x[i]<-x[i+1]
+x[i+1]<-t
+}
+for (i in 1:3){
+if(x[i]>x[i+1]){
+t<-x[i]
+x[i]<-x[i+1]
+x[i+1]<-t
+}
+for (i in 1:2){
+if(x[i]>x[i+1]){
+ t<-x[i]
+x[i]<-x[i+1]
+ x[i+1]<-t
+}
+if(x[1]>x[2]){
+t<-x[1]
+x[1]<-x[2]
+ x[2]<-t
+x
+}
+}
+}
+}
+x
